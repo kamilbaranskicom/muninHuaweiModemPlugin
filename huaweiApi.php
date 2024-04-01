b9916843a7606df66a2513ca502ba2f367c6f8b8
Kamil Barański <kamilbaranski@gmail.com>
2024-04-01 19:42:54 +0200

    open... and closeApiSession()

--- huaweiApi.php
+++ huaweiApi.php
@@ -4,38 +4,46 @@
 
 function getHuaweiDataFromApi($modemUrl = 'http://192.168.8.1', $apiMethod = 'api/monitoring/status') {
     global $PLUGINID;
-    $modemGetSessionUrl = $modemUrl . '/api/webserver/SesTokInfo';
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $modemGetSessionUrl);
-    curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-    curl_setopt($ch, CURLOPT_COOKIESESSION, true);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-    $result = curl_exec($ch);
-    if (curl_error($ch)) {
-        return ($PLUGINID . ' curl ERROR: ' . curl_error($ch) . chr(10));
-    };
-    $array = parseXmlToArray($result);
+    list($sesInfo, $tokInfo, $curlHandle) = openApiSession($modemUrl);
 
-    $sesInfo = $array['SesInfo'];
-    $tokInfo = $array['TokInfo'];
-
-    $modemStatusXmlUrl = $modemUrl . '/' . $apiMethod;
-    curl_setopt($ch, CURLOPT_URL, $modemStatusXmlUrl);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    curl_setopt($curlHandle, CURLOPT_URL, $modemUrl . '/' . $apiMethod);
+    curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array(
         'Cookie: ' . $sesInfo,
         '__RequestVerificationToken: ' . $tokInfo
     ));
 
-    $result = curl_exec($ch);
-    if (curl_error($ch)) {
-        return ($PLUGINID . ' curl ERROR: ' . curl_error($ch) . chr(10));
+    $result = curl_exec($curlHandle);
+    if (curl_error($curlHandle)) {
+        $curlError = curl_error($curlHandle);
+        closeApiSession($curlHandle);
+        return ($PLUGINID . ' curl ERROR: ' . $curlError . chr(10));
     };
 
+    closeApiSession($curlHandle);
     return parseXmlToArray($result);
-};
+}
 
+function openApiSession($modemUrl = 'http://192.168.8.1') {
+    $modemGetSessionUrl = $modemUrl . '/api/webserver/SesTokInfo';
+    $curlHandle = curl_init();
+    curl_setopt($curlHandle, CURLOPT_URL, $modemGetSessionUrl);
+    curl_setopt($curlHandle, CURLOPT_AUTOREFERER, true);
+    curl_setopt($curlHandle, CURLOPT_COOKIESESSION, true);
+    curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curlHandle, CURLOPT_CONNECTTIMEOUT, 5);
+    $result = curl_exec($curlHandle);
+    if (curl_error($curlHandle)) {
+        return ($PLUGINID . ' curl ERROR: ' . curl_error($curlHandle) . chr(10));
+    };
+    
+    $array = parseXmlToArray($result);
 
+    return array($array['SesInfo'], $array['TokInfo'], $curlHandle);
+}
+
+function closeApiSession($curlHandle) {
+    return curl_close($curlHandle);
+}
 
 function parseXmlToArray($xmlData) {
     $xml = simplexml_load_string($xmlData, "SimpleXMLElement", LIBXML_NOCDATA);
